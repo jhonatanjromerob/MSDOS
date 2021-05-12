99bafb113e9efe99f8809b5c6c6ea77961c122c3
EJRB660K <jhonatan.romero@inmetrics.cl>
2021-05-11 21:56:04 -0400

    Commit

---
+++ src/test/java/com/tests/MainClass.java
@@ -0,0 +1,17 @@
+package com.tests;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Tests test = new Tests();
+		
+		test.Test01();
+		test.Test02();
+		test.Test03();
+		test.Test04();
+		test.Test05();
+
+	}
+
+}
